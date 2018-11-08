fa4bb7c37216992a5f28fd9b1dbd264fc0d03aa9
xieaoDev <abdelilah.6h@gmail.com>
2018-11-08 02:45:56 +0100

    ...

--- src/main/java/xieao/theora/common/ability/AbilityEnderMe.java
+++ src/main/java/xieao/theora/common/ability/AbilityEnderMe.java
@@ -4,4 +4,8 @@ import xieao.theora.api.player.ability.Ability;
 
 public class AbilityEnderMe extends Ability {
 
+    // TODO ender prearl dosn't dammage you
+    // TODO doubl right click to teleport
+    // TODO
+
 }
--- src/main/java/xieao/theora/common/ability/AbilityTheGhost.java
+++ src/main/java/xieao/theora/common/ability/AbilityTheGhost.java
@@ -5,7 +5,7 @@ import xieao.theora.api.player.ability.Ability;
 public class AbilityTheGhost extends Ability {
 
     // TODO when invisible means invisible
-    // TODO
+    // TODO you can see invisible entities
     // TODO
     // TODO
     // TODO

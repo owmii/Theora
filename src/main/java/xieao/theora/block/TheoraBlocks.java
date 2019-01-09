192583bb4a26e6a0a73d0e2f303e3afd131c3bfe
xieaoDev <abdelilah.6h@gmail.com>
2019-01-09 16:18:35 +0100

    Re-ranging.

--- src/main/java/xieao/theora/block/TheoraBlocks.java
+++ src/main/java/xieao/theora/block/TheoraBlocks.java
@@ -1,23 +1,24 @@
 package xieao.theora.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import xieao.lib.block.IBlock;
-import xieao.theora.item.TheoraItems;
+import xieao.theora.Theora;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TheoraBlocks {
 
-    public static final List<Block> BLOCKS = new ArrayList<>();
     public static final List<ItemBlock> ITEM_BLOCKS = new ArrayList<>();
+    public static final List<Block> BLOCKS = new ArrayList<>();
 
     static <T extends Block & IBlock> T register(String name, T block) {
-        ItemBlock itemBlock = block.getItemBlock(TheoraItems.MAIN);
+        ItemBlock itemBlock = block.getItemBlock(new Item.Builder().group(Theora.MAIN));
         itemBlock.setRegistryName(name);
-        block.setRegistryName(name);
         ITEM_BLOCKS.add(itemBlock);
+        block.setRegistryName(name);
         BLOCKS.add(block);
         return block;
     }
--- src/main/java/xieao/theora/item/TheoraItems.java
+++ src/main/java/xieao/theora/item/TheoraItems.java
@@ -2,7 +2,6 @@ package xieao.theora.item;
 
 import net.minecraft.item.Item;
 import xieao.lib.item.IItem;
-import xieao.theora.Theora;
 import xieao.theora.block.TheoraBlocks;
 
 import java.util.ArrayList;
@@ -11,8 +10,6 @@ import java.util.List;
 public class TheoraItems {
 
     public static final List<Item> ITEMS = new ArrayList<>(TheoraBlocks.ITEM_BLOCKS);
-    public static final Item.Builder MAIN = new Item.Builder().group(Theora.MAIN);
-
 
     static <T extends Item & IItem> T register(String name, T item) {
         item.setRegistryName(name);

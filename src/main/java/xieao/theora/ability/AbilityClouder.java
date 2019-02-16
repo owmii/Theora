edc9071d72327921ac8598b379a42e9f974092ab
xieaoDev <12222738+xieaoDev@users.noreply.github.com>
2019-02-16 12:20:42 +0100

    Added some empty abilities.

---
+++ src/main/java/xieao/theora/ability/AbilityClouder.java
@@ -0,0 +1,6 @@
+package xieao.theora.ability;
+
+import xieao.theora.api.player.Ability;
+
+public class AbilityClouder extends Ability {
+}
--- src/main/java/xieao/theora/ability/AbilityMePig.java
+++ src/main/java/xieao/theora/ability/AbilityMePig.java
@@ -7,16 +7,13 @@ import xieao.theora.api.player.Ability;
 public class AbilityMePig extends Ability {
     @Override
     public void tick(EntityPlayer player, Data data) {
-        super.tick(player, data);
     }
 
     @Override
-    public void onAdded(EntityPlayer player, NBTTagCompound abilityNBT) {
-        super.onAdded(player, abilityNBT);
+    public void onAdded(EntityPlayer player, NBTTagCompound nbt) {
     }
 
     @Override
-    public void onRemoved(EntityPlayer player) {
-        super.onRemoved(player);
+    public void onRemoved(EntityPlayer player, NBTTagCompound nbt) {
     }
 }
